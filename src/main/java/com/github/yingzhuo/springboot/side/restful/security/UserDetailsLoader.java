8d27b7094c63df5f7e235a4828503059eef1cdab
Bill Ying <yingzhor@gmail.com>
2016-06-05 11:55:32 +0800

    update

--- src/main/java/com/github/yingzhuo/springboot/side/restful/security/UserDetailsLoader.java
+++
@@ -1,8 +0,0 @@
-package com.github.yingzhuo.springboot.side.restful.security;
-
-@FunctionalInterface
-public interface UserDetailsLoader {
-
-    public UserDetails load(AccessToken accessToken);
-
-}

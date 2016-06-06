2255c02b3c7cdcf13176f9cb75cf9073c66c97e7
Bill Ying <yingzhor@gmail.com>
2016-06-06 10:04:55 +0800

    update

--- springboot-side-core/src/main/java/com/github/yingzhuo/springboot/side/logging/LoggerBeanConfiguration.java
+++ springboot-side-core/src/main/java/com/github/yingzhuo/springboot/side/logging/LoggerBeanConfiguration.java
@@ -1,25 +1,56 @@
 package com.github.yingzhuo.springboot.side.logging;
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
+
+import java.io.Serializable;
 
 @ConditionalOnClass(Logger.class)
 @ConditionalOnMissingBean(LoggerBean.class)
 @ConditionalOnProperty(prefix = "springboot.side.logger-bean", name = "enabled", havingValue = "true", matchIfMissing = true)
-@EnableConfigurationProperties(LoggerBeanProperties.class)
+@EnableConfigurationProperties(LoggerBeanConfiguration.LoggerBeanProperties.class)
 public class LoggerBeanConfiguration {
 
-    @Autowired
-    private LoggerBeanProperties properties;
-
     @Bean
-    public LoggerBean loggerBean() {
+    public LoggerBean loggerBean(LoggerBeanProperties properties) {
         return new LoggerBeanImpl(properties.getName(), properties.getDefaultLevel());
+    }
+
+    @ConfigurationProperties(prefix = "springboot.side.logger-bean")
+    public static class LoggerBeanProperties implements Serializable {
+        private boolean enabled = true;
+        private String name = LoggerBean.class.getName();
+        private LogLevel defaultLevel = LogLevel.DEBUG;
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public LogLevel getDefaultLevel() {
+            return defaultLevel;
+        }
+
+        public void setDefaultLevel(LogLevel defaultLevel) {
+            this.defaultLevel = defaultLevel;
+        }
     }
 
 }
--- springboot-side-core/src/main/java/com/github/yingzhuo/springboot/side/profile/ProfileValidatorConfiguration.java
+++ springboot-side-core/src/main/java/com/github/yingzhuo/springboot/side/profile/ProfileValidatorConfiguration.java
@@ -1,39 +1,54 @@
 package com.github.yingzhuo.springboot.side.profile;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @ConditionalOnMissingBean(ProfileValidator.class)
 @ConditionalOnProperty(prefix = "springboot.side.profile-validator", name = "enabled", havingValue = "true", matchIfMissing = true)
-@EnableConfigurationProperties(ProfileValidatorProperties.class)
+@EnableConfigurationProperties(ProfileValidatorConfiguration.ProfileValidatorProperties.class)
 public class ProfileValidatorConfiguration {
 
-    @Autowired
-    private ProfileValidatorProperties properties;
-
-    @Autowired
-    private Environment environment;
-
     @Bean
-    public ProfileValidator profileValidator() {
-
+    public ProfileValidator profileValidator(ProfileValidatorProperties properties, Environment environment) {
         ProfileValidator bean = new ProfileValidator();
-
         bean.setCurrentProfiles(new HashSet<>(Arrays.asList(environment.getActiveProfiles())));
-
         for (Collection<String> group : properties.getMutualExclusionMap().values()) {
             bean.addMutualExclusion(new HashSet<>(group));
         }
-
         return bean;
     }
 
+    @ConfigurationProperties(prefix = "springboot.side.profile-validator")
+    public static class ProfileValidatorProperties {
+        private boolean enabled = true;
+        private Map<String, Set<String>> mutualExclusionMap = new HashMap<>();
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public Map<String, Set<String>> getMutualExclusionMap() {
+            return mutualExclusionMap;
+        }
+
+        public void setMutualExclusionMap(Map<String, Set<String>> mutualExclusionMap) {
+            this.mutualExclusionMap = mutualExclusionMap;
+        }
+
+    }
 }

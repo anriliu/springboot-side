43a92740444783e4da36513729958e5b63149027
Bill Ying <yingzhor@gmail.com>
2016-06-07 16:21:31 +0800

    update

--- springboot-side-qiniuyun/src/test/java/boot/ApplicationBoot.java
+++ springboot-side-qiniuyun/src/test/java/boot/ApplicationBoot.java
@@ -1,12 +1,15 @@
 package boot;
 
 import com.github.yingzhuo.springboot.side.qiniuyun.QiniuyunManager;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.io.InputStream;
 
 @SpringBootApplication
@@ -16,13 +19,15 @@ public class ApplicationBoot implements ApplicationRunner {
     private QiniuyunManager qiniuyunManager;
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationBoot.class, args);
+        SpringApplication.run(ApplicationBoot.class, args).close();
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String url = qiniuyunManager.upload((InputStream) null, "mock-key", true);
+        InputStream in = FileUtils.openInputStream(new File("/Users/yingzhuo/Desktop/avatar.jpg"));
+        String url = qiniuyunManager.upload(in, "mock-key", true);
         System.out.println(url);
+        IOUtils.closeQuietly(in);
     }
 
 }

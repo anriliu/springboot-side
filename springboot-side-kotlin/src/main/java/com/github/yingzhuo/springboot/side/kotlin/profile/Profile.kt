49952838f0dcd376fc0fa0204114e59f2a10867a
Bill Ying <yingzhor@gmail.com>
2016-06-15 17:09:28 +0800

    update

--- springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/profile/Profile.kt
+++ springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/profile/Profile.kt
@@ -5,3 +5,9 @@ import com.github.yingzhuo.springboot.side.profile.ProfileUtils
 inline fun inProfile(profile: String, lambda: () -> Unit): Unit {
     if (profile in ProfileUtils.getActiveProfilesAsSet()) lambda()
 }
+
+fun activeProfiles(): List<String> =
+        ProfileUtils.getActiveProfiles()
+
+fun isInProfile(profile: String): Boolean =
+        profile in activeProfiles()

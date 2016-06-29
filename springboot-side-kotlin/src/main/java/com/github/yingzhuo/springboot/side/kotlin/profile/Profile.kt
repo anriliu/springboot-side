b938a8c05f410497a94a151a581c85c0a27d717b
Bill Ying <yingzhor@gmail.com>
2016-06-29 13:20:13 +0800

    update

--- springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/profile/Profile.kt
+++ springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/profile/Profile.kt
@@ -8,6 +8,12 @@ inline fun ifProfileActived(profile: String, lambda: () -> Unit): Unit {
     }
 }
 
+inline fun ifAcceptsProfiles(vararg profiles: String, lambda: () -> Unit): Unit {
+    if (SpringUtils.acceptsProfiles(*profiles)) {
+        lambda()
+    }
+}
+
 fun activedProfiles(): List<String> = SpringUtils.getActivedProfiles()
 
 fun isProfileActived(profile: String): Boolean = SpringUtils.acceptsProfiles(profile)

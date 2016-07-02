d8a80b7a749a3944c1baefaa94821eaf6c3994f5
Bill Ying <yingzhor@gmail.com>
2016-07-02 09:51:59 +0800

    update

--- springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/RichString.kt
+++ springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/RichString.kt
@@ -2,6 +2,7 @@ package com.github.yingzhuo.springboot.side.kotlin
 
 import com.github.yingzhuo.springboot.side.util.EmojiUtils
 import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.time.DateUtils
 import java.nio.charset.Charset
 import java.util.*
@@ -19,6 +20,24 @@ inline fun String?.ifNotEmpty(lambda: (String) -> Unit): String? {
     }
     return this
 }
+
+fun String.abbreviate(maxSize: Int): String = StringUtils.abbreviate(this, maxSize)
+
+fun String.abbreviateMiddle(maxSize: Int, middle: String = "."): String = StringUtils.abbreviateMiddle(this, middle, maxSize)
+
+fun String.strip(chars: String): String = remove { it in chars }
+
+inline fun String.remove(lambda: (Char) -> Boolean): String = this.filterNot(lambda)
+
+fun String.removeWhitespace(): String = remove { it.isWhitespace() }
+
+fun String.removeSpace(): String = remove { it.equals(" ") }
+
+fun String.leftPad(size: Int, padString: String = " "): String = StringUtils.leftPad(this, size, padString)
+
+fun String.rightPad(size: Int, padString: String = " "): String = StringUtils.rightPad(this, size, padString)
+
+fun String.center(size: Int, padString: String = " "): String = StringUtils.center(this, size, padString)
 
 fun String.md5(): String =
         DigestUtils.md5Hex(this)

5ec8d3914d6dc6f310bdff6fca198f60d7fed750
Bill Ying <yingzhor@gmail.com>
2016-07-05 06:07:39 +0800

    update

--- springboot-side-core/src/main/java/com/github/yingzhuo/springboot/side/web/filter/BasePathSettingFilterProperties.java
+++ springboot-side-core/src/main/java/com/github/yingzhuo/springboot/side/web/filter/BasePathSettingFilterProperties.java
@@ -3,12 +3,20 @@ package com.github.yingzhuo.springboot.side.web.filter;
 import com.github.yingzhuo.springboot.side.web.config.AbstractFilterProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.core.Ordered;
 
 @ConditionalOnWebApplication
 @ConditionalOnProperty(prefix = "springboot.side.basepath-setting-filter", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class BasePathSettingFilterProperties extends AbstractFilterProperties {
 
     private String[] basepathAttributeNames = new String[] { "webroot", "WEBROOT", "basePath", "BASEPATH" };
+
+    public BasePathSettingFilterProperties() {
+        super.setEnabled(true);
+        super.setFilterName(BasePathSettingFilter.class.getSimpleName());
+        super.setFilterOrder(Ordered.LOWEST_PRECEDENCE);
+        super.setUrlPatterns(new String[] {"/*"});
+    }
 
     public String[] getBasepathAttributeNames() {
         return basepathAttributeNames;
@@ -17,4 +25,5 @@ public class BasePathSettingFilterProperties extends AbstractFilterProperties {
     public void setBasepathAttributeNames(String[] basepathAttributeNames) {
         this.basepathAttributeNames = basepathAttributeNames;
     }
+
 }

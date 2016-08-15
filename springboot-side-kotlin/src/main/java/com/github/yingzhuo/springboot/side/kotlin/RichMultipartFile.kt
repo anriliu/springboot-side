b38148209c71211cafecafdc9f164576be7a3871
yingzhuo <yingzhor@gmail.com>
2016-08-15 14:30:14 +0800

    update

--- springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/RichMultipartFile.kt
+++ springboot-side-kotlin/src/main/java/com/github/yingzhuo/springboot/side/kotlin/RichMultipartFile.kt
@@ -3,7 +3,7 @@ package com.github.yingzhuo.springboot.side.kotlin
 import org.springframework.web.multipart.MultipartFile
 
 inline fun MultipartFile?.ifPresent(lambda: (MultipartFile) -> Unit) {
-    if (this != null && !this.isEmpty) {
+    if (this != null && !this.isEmpty && this.size != 0L) {
         lambda(this)
     }
 }
